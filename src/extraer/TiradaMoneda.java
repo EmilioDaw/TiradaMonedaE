ef7214462610b43e83299cbed8b90049df67bbdb
Emilio <Emilio@192.168.1.34>
2020-03-02 22:42:34 +0100

    Refactorizacion Cambio Nombre del Metodo Ejercicio 5

--- src/extraer/TiradaMoneda.java
+++ src/extraer/TiradaMoneda.java
@@ -68,7 +68,7 @@ public class TiradaMoneda {
 				contador2 = 0;
 			}
 		}
-		imprimir(contadorCaras, contadorCruces, maximoCarasDoblesConsecutivas, maximoCrucesDoblesConsecutivas);
+		imprime(contadorCaras, contadorCruces, maximoCarasDoblesConsecutivas, maximoCrucesDoblesConsecutivas);
 	}
 
 	/**
@@ -76,8 +76,20 @@ public class TiradaMoneda {
 	 * @param contadorCruces
 	 * @param maximoCarasDoblesConsecutivas
 	 * @param maximoCrucesDoblesConsecutivas
+	 * @deprecated Use {@link #imprime(int,int,int,int)} instead
 	 */
 	public static void imprimir(int contadorCaras, int contadorCruces, int maximoCarasDoblesConsecutivas,
+			int maximoCrucesDoblesConsecutivas) {
+				imprime(contadorCaras, contadorCruces, maximoCarasDoblesConsecutivas, maximoCrucesDoblesConsecutivas);
+			}
+
+	/**
+	 * @param contadorCaras
+	 * @param contadorCruces
+	 * @param maximoCarasDoblesConsecutivas
+	 * @param maximoCrucesDoblesConsecutivas
+	 */
+	public static void imprime(int contadorCaras, int contadorCruces, int maximoCarasDoblesConsecutivas,
 			int maximoCrucesDoblesConsecutivas) {
 		System.out.println("Caras: " + contadorCaras + ", Cruces: " + contadorCruces);
 		System.out.println("Maximo de caras dobles consecutivas: " + maximoCarasDoblesConsecutivas);
